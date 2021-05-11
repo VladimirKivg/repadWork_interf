7f403844df52a673552603280a9a71b0b0b7d045
VladimirKing <cheremuha1@gmail.com>
2021-05-11 13:42:16 +0300

    work

--- src/RepaidWork.java
+++ src/RepaidWork.java
@@ -5,9 +5,9 @@ import repadWork_interf.Test2;
 public class RepaidWork {
     public static void main(String[] args) {
 
-        Mi82 mi8=new Mi82();
-        Boeing749 boeing747=new Boeing749();
-        Test2 test=new Test2();
+        Mi82 mi8 = new Mi82();
+        Boeing749 boeing747 = new Boeing749();
+        Test2 test = new Test2();
         test.foo(mi8);
         test.foo(boeing747);
     }
