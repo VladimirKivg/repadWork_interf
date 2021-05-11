c533bba3c3c8d7f997712818c1f40d63f0a7b9e6
VladimirKing <cheremuha1@gmail.com>
2021-05-11 13:42:05 +0300

    work

--- src/repadWork_interf/Helicopter2.java
+++ src/repadWork_interf/Helicopter2.java
@@ -1,9 +1,9 @@
 package repadWork_interf;
 
-public abstract class Helicopter2 extends FlingObject2 implements VerticalTakeoff2{//и этот тагже повторно сделаем абстрактным
+public abstract class Helicopter2 extends FlingObject2 implements VerticalTakeoff2 {//и этот тагже повторно сделаем абстрактным
 //Helicopter уже наследуеться от одного сласса, а в java множественное наследование запрещено
-/* для ветрикального взлета мы создадим интерфейс так как вертикально может взлетать не только
-* вертолет но и допустим ракета */
+    /* для ветрикального взлета мы создадим интерфейс так как вертикально может взлетать не только
+     * вертолет но и допустим ракета */
 
 
 }
